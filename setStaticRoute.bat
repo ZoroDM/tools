b8978085d431a00c35d5482ab7103aa5684ee634
sudm <sudm@yealink.com>
2019-05-25 10:22:47 +0800

    更新支持windows平台同时使用无线网卡和有线网卡的路由设置脚本，当无线网络未连接是外网依然走有线

--- setStaticRoute.bat
+++ setStaticRoute.bat
@@ -1,4 +1,5 @@
 route print
 route delete 0.0.0.0
-route -p add 10.0.0.0 mask 255.0.0.0 10.85.1.254
-route -p add 0.0.0.0 mask 0.0.0.0 172.20.10.1
+route -p add 10.0.0.0 mask 255.0.0.0 10.85.1.254 metric 1
+route -p add 0.0.0.0 mask 0.0.0.0 172.20.10.1 metric 1
+route -p add 0.0.0.0 mask 0.0.0.0 10.85.1.254 metric 2
